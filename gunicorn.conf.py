16c0e662edcf40ac204a105632fdc5a7b091579b
Tri Nguyen <tritn928@gmail.com>
2025-06-20 23:36:14 -0500

    Updated docker-compose

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,6 @@
 loglevel = "info"
 workers = 1  # Using 1 worker is crucial for low-memory environments like the free tier
 threads = 2  # Each worker can handle multiple requests concurrently
-bind = "0.0.0.0:80"
+bind = "0.0.0.0:5000"
 keepalive = 120
 timeout = 120
